88537ba4ab62c24b12ab4b6d483b6347a6183aaa
Marin Clement <clement.marin5@etu.univ-lorraine.fr>
2026-06-01 10:04:10 +0200

    modif de mainperso

--- projet_zeldiablo/src/MainPerso.java
+++ projet_zeldiablo/src/MainPerso.java
@@ -3,6 +3,5 @@ import moteurJeu.MoteurGraphique;
 public class MainPerso {
     JeuPerso jeu = new JeuPerso();
     DessinPerso dessin = new DessinPerso(jeu);
-    MoteurGraphique moteur = new MoteurGraphique(jeu, dessin);
-    moteur.lancerJeu(400, 400);
+    MoteurGraphique m = new MoteurGraphique(jeu, dessin);
 }
