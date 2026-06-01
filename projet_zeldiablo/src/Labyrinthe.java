737c518fc20b12c044724e5b63559ceaaa3865c7
Marin Clement <clement.marin5@etu.univ-lorraine.fr>
2026-06-01 11:10:26 +0200

    modification pour afficher une grille grise avec une bordure noir

---
+++ projet_zeldiablo/src/Labyrinthe.java
@@ -0,0 +1,25 @@
+public class Labyrinthe {
+
+    public static final int LARGEUR = 10;
+    public static final int HAUTEUR = 10;
+
+    private boolean[][] mur;  // attribut direct
+
+    public Labyrinthe() {
+        mur = new boolean[LARGEUR][HAUTEUR];
+
+        // murs sur les bordures
+        for (int x = 0; x < LARGEUR; x++) {
+            mur[x][0] = true;
+            mur[x][HAUTEUR - 1] = true;
+        }
+        for (int y = 0; y < HAUTEUR; y++) {
+            mur[0][y] = true;
+            mur[LARGEUR - 1][y] = true;
+        }
+    }
+
+    public boolean estMur(int x, int y) {
+        return mur[x][y];
+    }
+}
