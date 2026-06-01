09b331723a3a4dad23f6c538df0ce653af21ba8a
Lilian Szelest <szelest.lilian@gmail.com>
2026-06-01 09:30:06 +0200

    ajout de la classe DessinPerso

---
+++ projet_zeldiablo/src/DessinPerso.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+
+
+public class DessinPerso implements DessinJeu{
+
+    public static final int TAILLE = 40;
+
+    private JeuPerso jeuEnCours;
+
+    public DessinPerso(JeuPerso jeuPerso){
+        this.jeuEnCours = jeuPerso;
+    }
+
+    public void dessiner(BufferedImage image){
+        Graphics2D g = (Graphics2D) image.getGraphics();
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(0, 0, image.getWidth(), image.getHeight());
+
+        Personnage pj = this.jeuEnCours.getPj();
+        g.setColor(Color.BLUE);
+        g.fillOval(pj.getX() * TAILLE, pj.getY() * TAILLE, TAILLE, TAILLE);
+
+        g.dispose();
+    }
+}
